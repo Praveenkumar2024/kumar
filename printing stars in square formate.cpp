d17e716f3cc50f9135b3ddbde1646d88bd1dcf1f
Praveenkumar2024 <130967293+Praveenkumar2024@users.noreply.github.com>
2023-04-17 23:46:40 +0530

    Create printing stars in square formate.cpp

---
+++ printing stars in square formate.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n=3;
+  for(int i=1;i<=n;i++)
+  {
+    for(int j=1;j<=3;j++)
+    {
+      cout<<"* ";
+    }
+    cout<<endl;
+  }
+}
