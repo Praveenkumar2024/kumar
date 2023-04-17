9011c393dae64cc7758d49bf72b3b29b154c0ec2
Praveenkumar2024 <130967293+Praveenkumar2024@users.noreply.github.com>
2023-04-17 23:42:24 +0530

    Create max num in two num.cpp

---
+++ max num in two num.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a=50;
+  int b=60;
+  if(a>b)
+  {
+    cout<<a<<" is big number"<<endl; 
+  }
+  else
+  {
+    cout<<b<<" is big number"<<endl;
+  }
+}
